58fed8e91dbfbd7673f9a598a3de8a7108173a5e
Marco Antonio Muñiz Ochoa <marco.muniz.ochoa@gmail.com>
2012-11-10 22:09:14 -0600

    Issue #2: Add restGithubClient to spring beans

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,17 @@
 // Place your Spring DSL code here
 beans = {
+  httpClient(wslite.http.HTTPClient) {
+    connectTimeout = 5000
+    readTimeout = 10000
+    useCaches = false
+    followRedirects = false
+    sslTrustAllCerts = true
+  }
+
+  restGithubClient(wslite.rest.RESTClient) {
+    url = 'https://github.com/'
+    httpClient = ref('httpClient')
+    defaultAcceptHeader = 'application/json'
+    defaultCharset = 'UTF-8'
+  }
 }
