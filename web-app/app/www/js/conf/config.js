a319115e0942ba1e9eb4dca98897304144800a95
Marco Antonio Muñiz Ochoa <marco.muniz.ochoa@gmail.com>
2012-11-10 20:46:12 -0600

    Issue #2: Add properties in config

--- web-app/app/www/js/conf/config.js
+++ web-app/app/www/js/conf/config.js
@@ -2,7 +2,10 @@ define([
 ], function(){
 
   var config = {};
+  config.home = '/trollboard'
   config.log = true;
+
+  config.urlVerifySession = "http://localhost/trollboard/session";
 
   return config;
 });
