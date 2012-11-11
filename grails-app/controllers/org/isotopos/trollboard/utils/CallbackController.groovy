e62f2e331c6786047b59226eaa0739bed49c9dd5
neodevelop <neodevelop@gmail.com>
2012-11-10 18:34:38 -0600

    WIP #7 Recieivin payload and processing messages

--- grails-app/controllers/org/isotopos/trollboard/utils/CallbackController.groovy
+++ grails-app/controllers/org/isotopos/trollboard/utils/CallbackController.groovy
@@ -2,9 +2,20 @@ package org.isotopos.trollboard.utils
 
 class CallbackController {
 
+  def commitService
+
   def index() {
   	params.remove 'controller'
   	session.user = params
+    def payload = params.payload
+    def providerId = params.providerId
+    Project project = Project.findByProviderIdAndProjectId(providerId,payload.repository.name)
+    if(project){
+      // TODO: Process commit
+      payload.commits.each{ commit ->
+        def actions = commitService.receiveAndProcessMessage(commit.message)
+      }
+    }
   	println session.user
   	redirect uri: '/app/www'
   }
