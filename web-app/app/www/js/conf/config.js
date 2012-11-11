30f437d530846ecbcc4055effdc0396f5ff302b3
Marco Antonio Muñiz Ochoa <marco.muniz.ochoa@gmail.com>
2012-11-11 07:48:16 -0600

    Fix config

--- web-app/app/www/js/conf/config.js
+++ web-app/app/www/js/conf/config.js
@@ -6,7 +6,7 @@ define([
   config.home = 'trollboard';
   config.log = true;
 
-  config.urlVerifySession = 'http://trollboard.rs.af.cm/trollboard/session';
+  config.urlVerifySession = 'http://trollboard.rs.af.cm/session';
 
   config.profile = 'http://trollboard.rs.af.cm/v1/user?providerId=github&providerToken='
   config.project = 'http://trollboard.rs.af.cm/v1/user/projects?providerId=github&providerToken='
