2319310f08f22141f9d0c77ba0365f1fc04f70ee
Marco Antonio Muñiz Ochoa <marco.muniz.ochoa@gmail.com>
2012-11-11 13:08:33 -0600

    Remove println

--- grails-app/controllers/org/isotopos/trollboard/StartController.groovy
+++ grails-app/controllers/org/isotopos/trollboard/StartController.groovy
@@ -6,9 +6,8 @@ class StartController {
 
   def profile() {
     def trollboardProfile = session.trollboardProfile
-    println "Trollboard Profile: $trollboardProfile"
     def profile = apiUserProfileService.getUserProfile(trollboardProfile.provider_id, trollboardProfile.access_token)
-    println "PROFILE STARTS: ${profile}"
+
     [profile: profile]
   }
 }
