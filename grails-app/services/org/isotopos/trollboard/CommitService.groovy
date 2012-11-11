7165e33cc45d27f7e8450ea503cb95789b38f563
fjuarezmurillo <felipe.juarez@synergyj.com>
2012-11-10 21:04:35 -0600

    #5 Procesando mensajes

--- grails-app/services/org/isotopos/trollboard/CommitService.groovy
+++ grails-app/services/org/isotopos/trollboard/CommitService.groovy
@@ -7,30 +7,32 @@ class CommitService {
 
     if(message) {
       // "Fix #1 commit message"
-      def rawIssueLabels = message =~ /\w+\D.[?=\s#]/
+      def rawIssueLabels = message =~ /\w+\D.[?=#]/
       def issueLabels = []
-      rawIssueLabels.each { it -> 
-        issueLabels << it.replace(" #",'') 
+      rawIssueLabels.each { label ->
+        issueLabels << (label?.replace(" #",'')?.trim())?.toUpperCase()
       }
 
-      def labelsAndIssues = [:]
       int index = 0;
       issueLabels.each { label ->
-        def start = message.indexOf(label)
-        def end = message.length()
+        def start = message.toUpperCase().indexOf(label)
+        def end = message.toUpperCase().length()
         if( issueLabels.size() > (index+1) ){
-          end = message.indexOf(issueLabels.get(++index))
+          end = message.toUpperCase().indexOf(issueLabels.get(++index))
         }
 
-        def issueNumbers = message.substring(start, end) =~ /#\d+/
+        def issueNumbers = message.toUpperCase().substring(start, end) =~ /#\d+/
         def issuesNumbers = []
         issueNumbers.each{ issue ->
-          issuesNumbers << [issue]
+          issuesNumbers << issue
         }
-        labelsAndIssues."$label" = issuesNumbers
-      }
 
-      issueStatus 
+        def labelsAndIssues = [:]
+        labelsAndIssues."${label}" = issuesNumbers
+        issueStatus << labelsAndIssues
+      }
     }
+
+    issueStatus
   }
 }
