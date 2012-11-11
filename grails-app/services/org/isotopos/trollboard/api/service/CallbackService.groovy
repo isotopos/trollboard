54923876424262149cadfc5fa883ac943be91c3c
neodevelop <neodevelop@gmail.com>
2012-11-11 06:51:05 -0600

    closes #14 Implementing service

--- grails-app/services/org/isotopos/trollboard/api/service/CallbackService.groovy
+++
@@ -1,8 +0,0 @@
-package org.isotopos.trollboard.api.service
-
-class CallbackService {
-
-    def serviceMethod() {
-
-    }
-}
---
+++ grails-app/services/org/isotopos/trollboard/api/service/GitHubCallbackService.groovy
@@ -0,0 +1,30 @@
+package org.isotopos.trollboard.api.service
+
+import org.isotopos.trollboard.Project
+
+class GitHubCallbackService implements CallbackService {
+
+  def gitHubIssuesService
+  def commitService
+
+  Project processPayload(String tokenProvider, String providerId, payload) {
+    Project project = Project.findByProviderIdAndProjectId(providerId,payload.repository.name)
+    if(project){
+      payload?.commits?.each{ commit ->
+        def actions = commitService.receiveAndProcessMessage(commit.message)
+        actions.each{ action ->
+          action.each { k,v ->
+            v.each { issueNumber ->
+              gitHubIssuesService.addLabelToIssue(
+                project?.token ?: tokenProvider,
+                payload?.repository?.owner?.name,
+                payload?.repository?.name,
+                issueNumber.replace("#","").trim(),k)
+            }
+          }
+        }
+      }
+    }
+    project
+  }
+}
