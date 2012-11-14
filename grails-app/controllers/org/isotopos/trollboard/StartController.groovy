3dc5af0f479cd0cf6a51163fa558713f6f6c3751
neodveelop <neodevelop@gmail.com>
2012-11-13 22:42:40 -0600

    #17 Actualizando el accessToken de la BD si ha cambiado

--- grails-app/controllers/org/isotopos/trollboard/StartController.groovy
+++ grails-app/controllers/org/isotopos/trollboard/StartController.groovy
@@ -20,6 +20,16 @@ class StartController {
     def projects
     try{
       projects = apiUserProfileService.getProjects(providerId, accessToken)
+
+      projects.each{ p ->
+        org.isotopos.trollboard.Project project = org.isotopos.trollboard.Project.findByProviderIdAndProjectId(providerId,p.name)
+        if(project){
+          if(project.token != accessToken){
+            project.token = accessToken
+            project.save(flush:true)
+          }
+        }
+      }
     }catch (e){
       log.error("ERROR get projects", e)
     }
